96be05456abe8b0c0c7aa56f5091408b4edc39a5
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2024-12-20 18:27:01 +0600

    Add files via upload

--- ReadyHamza.cpp
+++ ReadyHamza.cpp
@@ -1,104 +1,5 @@
-/*
-
-                                                                                                                                            ░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒░░░░
-    ░░▒▒▓▓▓▓▓▓▒▒▒▒▒▒░░                                                                                                            ░░▒▒▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████████▓▓▒▒
-  ▒▒██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒                                                                                                    ▒▒▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓██████████▓▓▓▓▓▓▓▓▒▒
-░░░░░░▒▒██▓▓██████▓▓▓▓▓▓▓▓██▓▓                                                                                            ▒▒▓▓██▓▓▒▒▓▓▓▓▓▓▓▓▓▓████████▓▓▓▓▓▓▓▓▓▓▒▒
-          ░░██▓▓██████▓▓▓▓▓▓▓▓▓▓▒▒                                                                                    ▓▓██▓▓▓▓▒▒▓▓▓▓▓▓▓▓██████████████▓▓▓▓▒▒
-              ▒▒██▓▓████▓▓▓▓██▓▓▓▓▓▓░░                                                                            ▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓██████████████▓▓██░░
-                  ▒▒▓▓████▓▓▓▓▓▓▓▓▓▓▒▒▒▒                                                ▒▒                    ▒▒▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓████████████████
-                    ░░██████▓▓▒▒▓▓██▓▓▓▓▒▒            ▒▒                                ▒▒                ░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████████████░░
-                        ██████▓▓▒▒▓▓██▓▓▓▓▓▓            ░░                              ▓▓              ▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████▓▓▓▓▓▓░░
-                          ██████▓▓▒▒▓▓▓▓▓▓▓▓▓▓          ██                              ▓▓            ▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▒▒
-                            ██████▓▓▒▒▓▓▓▓▓▓▓▓▓▓░░        ▒▒                            ▓▓        ░░▒▒░░▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒
-                            ░░████▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓░░      ▓▓                ░░          ▓▓      ▒▒▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░
-                                ████▓▓▓▓▓▓▒▒░░▓▓▓▓▓▓      ░░▓▓              ▓▓        ░░▓▓  ░░░░▒▒▓▓▒▒▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▒▒
-                        ▒▒▓▓▓▓████████▓▓▓▓▓▓▒▒▒▒▒▒▓▓▓▓      ▓▓              ▒▒▒▒      ▒▒▓▓  ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒
-                    ▒▒▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▒▒▓▓▒▒▓▓▒▒▒▒▓▓▓▓    ▓▓▓▓            ▒▒▓▓      ▓▓▓▓  ▓▓▓▓▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░
-                  ▓▓▓▓▓▓▓▓██▓▓▒▒▓▓▓▓████▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓  ░░▓▓  ░░        ░░▓▓      ▓▓▓▓  ▒▒▒▒▓▓████▓▓▓▓▓▓▓▓▒▒▒▒░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████████████▓▓▓▓▓▓▓▓▒▒
-                ██████████████████▓▓▓▓████▓▓▓▓▒▒▒▒▓▓▒▒▓▓▓▓░░  ▓▓▒▒░░        ▒▒▓▓▓▓  ▒▒▓▓▓▓▒▒▒▒████▓▓▓▓▓▓████████▓▓▒▒▓▓▓▓▓▓▓▓▓▓██████████████████████████▓▓▓▓▓▓
-              ▒▒▓▓██████████████████▓▓████▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒  ▓▓██░░░░      ▓▓▓▓▓▓  ▓▓▓▓▓▓▒▒▓▓██▓▓▓▓▓▓████████▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓████████████▓▓▓▓████▓▓▓▓██████▓▓▓▓
-              ▓▓██▓▓▒▒▒▒▒▒████████████████▓▓████▓▓▒▒▒▒▓▓▓▓██  ▒▒▓▓░░░░    ▓▓████▓▓░░▓▓▓▓░░▒▒██████████▓▓██████▓▓▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓
-                                  ░░▓▓██████▓▓▓▓████▓▓▒▒▓▓▓▓▓▓░░▒▒▓▓    ▓▓░░████▓▓▓▓▒▒▒▒  ▒▒▒▒▒▒████▓▓▒▒▓▓▓▓████▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒  ░░░░▒▒▒▒▓▓██▓▓▓▓████████▓▓
-                              ▓▓▓▓▓▓▓▓▓▓██████▓▓██▓▓████▒▒██▓▓▒▒▓▓██  ▓▓██▓▓▓▓▓▓▓▓▒▒▒▒▓▓▒▒    ██▓▓▓▓██████████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒                        ░░▒▒██▓▓▓▓▓▓
-                            ██▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▒▒▓▓████▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓▓▓▓██▒▒▓▓████▓▓▓▓    ██▒▒  ▒▒▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                              ▓▓▓▓▒▒
-                          ▓▓▓▓▓▓██████████▓▓▓▓██████▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓██▒▒▓▓  ▒▒██▒▒  ▒▒██▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒▓▓░░                              ▓▓
-                        ░░▓▓▓▓████████████████▓▓▓▓████▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▒▒▓▓░░▓▓▒▒  ▒▒██▓▓▓▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▒▒▓▓                                ░░
-                        ▓▓▓▓██████████████████████▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓██▓▓▓▓▒▒░░▓▓▓▓▓▓▓▓▓▓██▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▓▓▓▓
-                        ▓▓████████▒▒░░        ░░▓▓██████▓▓██▒▒▓▓▓▓▓▓▒▒▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▒▒▒▒░░      ▒▒▓▓██▓▓████▓▓▓▓▓▓
-                      ░░▓▓▓▓▓▓▓▓                    ▒▒▓▓██▓▓▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▒▒▒▒  ▒▒                ▒▒▓▓▓▓▓▓▓▓▓▓░░
-                      ░░██▓▓▒▒                        ░░████▓▓▓▓▓▓██████▓▓▓▓▓▓████▓▓████▓▓▓▓▓▓██████▓▓▒▒░░▒▒▓▓                      ▒▒▓▓▓▓██▓▓
-                      ░░▓▓▓▓                          ▓▓▓▓██▓▓▓▓██████████████▓▓▓▓▓▓▓▓████████▓▓▓▓▒▒▓▓▓▓▓▓▒▒                            ▓▓▓▓▓▓▓▓
-                      ░░▓▓░░                          ▓▓▓▓██▓▓▓▓██▓▓██████████▓▓▒▒▓▓▓▓▓▓████▓▓██▓▓▓▓▓▓▒▒                                  ▒▒▓▓██▒▒
-                        ▓▓                            ██▓▓██▓▓▓▓▒▒  ████▓▓████▒▒▓▓▒▒▓▓▓▓██▓▓████▓▓▓▓▓▓▓▓    ▒▒                                ▓▓▓▓
-                        ▓▓                            ▓▓▓▓▓▓▓▓▓▓    ░░▓▓██▓▓▓▓▓▓▓▓▓▓▓▓██████▓▓████▓▓▓▓██    ██                                  ▒▒░░
-                        ░░                            ▓▓██▓▓▓▓▓▓░░    ▓▓▓▓▓▓▓▓██▓▓▓▓██▓▓▓▓██████▓▓▒▒▓▓▓▓░░░░▓▓                                    ▓▓
-                                              ░░      ▓▓██▓▓██▓▓▓▓    ░░██▓▓▓▓▓▓▓▓▓▓██▓▓██▓▓████▓▓▒▒▒▒▓▓▒▒▓▓▒▒                                      ░░
-                                              ░░░░    ▒▒██▓▓▓▓▓▓██░░▒▒  ██▓▓▓▓░░▓▓██▓▓██▓▓██▓▓▓▓▓▓▓▓▒▒▒▒████
-                                                ▒▒▒▒    ▓▓██▓▓▓▓▒▒▓▓▓▓  ▓▓▓▓░░      ▓▓▒▒▓▓▓▓▓▓██▓▓▓▓▒▒▒▒▓▓▓▓
-                                                  ▓▓▓▓▒▒▓▓██▓▓▓▓▓▓▓▓▓▓  ░░▒▒          ▓▓██▓▓██▓▓▓▓▓▓▒▒▓▓▓▓▓▓
-                                                    ▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▒▒              ░░▓▓▓▓████▓▓██▓▓▓▓▓▓▓▓▓▓
-                                                        ▓▓████▒▒▓▓▓▓▒▒              ▒▒▓▓██████▓▓████▓▓▓▓▓▓▓▓
-                                                          ████▓▓▓▓▓▓▓▓              ▓▓▓▓██████  ████▓▓▓▓▒▒▓▓░░
-                                                          ▓▓▓▓▒▒▓▓▓▓▓▓▓▓          ▒▒▓▓██████  ▒▒  ▓▓▓▓▒▒▒▒▓▓▓▓
-                                                          ▓▓▓▓▓▓██▓▓████▒▒        ▓▓▒▒██▓▓▓▓████░░██████▓▓██▓▓
-                                                            ▓▓▓▓▓▓  ▒▒▓▓▓▓        ▓▓████▓▓████▓▓  ▓▓██▓▓▓▓▓▓▓▓▒▒
-                                                            ░░▓▓██    ▒▒▓▓        ▓▓██▓▓░░████    ██▓▓██▓▓▓▓▓▓▓▓▓▓
-                                                              ▓▓▓▓      ░░        ▒▒██░░░░▓▓▓▓    ██▓▓██  ▓▓▒▒██▓▓██
-                                                              ▓▓▓▓                ░░    ▓▓██▒▒  ░░▓▓▓▓██      ██▓▓▓▓
-                                                                ▓▓                    ▒▒▓▓▓▓▒▒  ░░▓▓▓▓▒▒      ▓▓▓▓▓▓
-                                                                                  ░░██▓▓▓▓██    ██▓▓▓▓        ▒▒▓▓▓▓▒▒
-                                                                      ░░        ████▓▓▓▓██      ██▓▓▓▓        ▒▒▓▓▓▓▓▓
-                                                                              ▒▒░░▓▓▒▒██░░      ▓▓▓▓▒▒        ▒▒▓▓▓▓▓▓▓▓
-                                                                    ██          ▒▒  ▒▒        ▒▒██▓▓          ▓▓▓▓▓▓██▓▓░░
-                                                                    ▓▓              ▒▒        ██▓▓▒▒        ░░▓▓▓▓▓▓▓▓▓▓▒▒
-                                                                    ▓▓                      ░░██▓▓░░        ▒▒▓▓██▓▓▒▒▓▓▓▓
-                                                                    ▓▓                    ▒▒▓▓▓▓▓▓          ▓▓██▒▒▓▓░░▓▓▓▓
-                                                                    ▓▓░░                  ████▓▓            ▓▓░░  ▓▓    ▓▓
-                                                                    ▒▒▓▓              ░░  ████▒▒            ▓▓    ▓▓    ▒▒
-                                                                    ░░▓▓              ▒▒▓▓██▓▓              ▒▒    ▓▓
-                                                                      ▓▓▓▓░░░░░░  ▒▒▒▒██▓▓██                ░░    ░░
-                                                                      ░░▓▓▓▓▒▒▓▓▒▒▓▓▓▓██▓▓
-                                                                        ▒▒▓▓▓▓▓▓▓▓▓▓▓▓░░
-
-*/
-//#include<bits/stdc++.h>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <cstdlib>
-#include <vector>
-#include <map>
-#include <queue>
-#include <stack>
-#include <ctype.h>
-#include <cctype>
-#include <set>
-#include <bitset>
-#include <algorithm>
-#include <list>
-#include <utility>
-#include <functional>
-#include <deque>
-#include <numeric>
-#include <iomanip>
-#include <cstdio>
-#include <cstring>
-#include <ctime>
-#include <assert.h>
-
-#include <unordered_set>
-#include <unordered_map>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-#include<cmath>
-#include<math.h>
-
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<ctype.h>
 
 //For Debugging
 #define debug(a...)          {cout<<__LINE__<<" #--> ";dbg,a; cout<<endl;}
@@ -112,12 +13,14 @@ struct debugger
 } dbg;
 
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<int,null_type,less<int>,rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;//exmp:indexed_set st
-#define index(s,k) s.find_by_order(k);///  who is k'th position
+template<class T>using iset=tree<T,null_type,less<T>,///descending_order=greater<T>,multi_iset=less_equal<T>
+rb_tree_tag,tree_order_statistics_node_update> ;///exmp= iset<double>st
+///who is k'th position= *(set.find_by_order(k)),index of v = set.order_of_key(v);
 
-typedef long long ll;
+typedef long long ll;/// 1e18;
+//typedef __int128_t LL;/// 1e32;
 template <typename T>
 T BIGMOD(T base,T power,T Mod=1e9+7){T ans = 1;
 while(power){if(power & 1)ans = (ans * base) % Mod;
@@ -129,10 +32,6 @@ T INV(T base,T mod=1e9+7){//defult mod=1e9+7
 }
 
 template <typename T>
-vector<T>primeFactors(T n){vector<T>PRIME;T c=2;while///prime factor
-(n>1){if(n%c==0){PRIME.push_back(c);n/=c;}else c++;}return PRIME;}
-
-template <typename T>
 T nCr(T n, T r){T res=1;for (T i=0;i<r;i++){
 res *= (n-i);res /= (i+1);}return res;}/// NCR
 
@@ -142,42 +41,58 @@ T fact(T n){if(n<=1)return 1;return n*fact(n-1);}
 template <typename T>
 T nPr(T n, T r){return fact(n)/fact(n-r);}///NPR
 
+
 #define deb(a)     cout<<__LINE__<<"# "<<#a<<" -> "<<a<<endl;
 #define all(x) x.begin(), x.end()
+#define loop(IV,INIT,NV) for(int IV=INIT;IV<=NV;IV++)
 #define lbv(vec,x) lower_bound(all(vec),x)-vec.begin()//retrun index
 #define ubv(vec,x) upper_bound(all(vec),x)-vec.begin()
-#define lba(ar,n,x) lower_bound(ar,ar+n,x)-&ar[0];//return index        
-#define uba(ar,n,x) upper_bound(ar,ar+n,x)-&ar[0]; 
+#define lba(ar,n,x) lower_bound(ar,ar+n,x)-&ar[0];//return index
+#define uba(ar,n,x) upper_bound(ar,ar+n,x)-&ar[0];
 #define lb(mp,x) mp.lower_bound(x);///for set,map
 #define ub(st,x) st.upper_bound(x);///return pointer address
 #define GCD(a,b)  __gcd(a,b)
 #define LCM(a,b)  (a*b)/__gcd(a,b)
 #define sqr(a)    ((a)*(a))
-#define sp(n,x)  fixed << setprecision(x) <<n
+#define deg(n)  n*PI/180 /// redian to degree
+#define sp(n,d)  fixed << setprecision(d) <<n
 #define pb emplace_back
+#define F  first
+#define S  second
 #define ss " "
 #define nn printf("\n");
 #define el cout<<"\n";
 #define yes cout<<"Yes\n";
 #define no cout<<"No\n";
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
 
 template <typename T>
 using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 const double PI = acos(-1);
 const double EPS = 1e-7; ///1*10^-7
-const int oo = 2e9+10;
-const ll MOD = 1e9+7;// Prime
+const int oo = 1e9+10;
+const ll MOD = 1e9 +7;// Prime
 
-int main()
+void solve(){
+
+}
+
+signed main()
 {
-//   freopen("in.txt", "r", stdin);  ///To read from a file.
-//    freopen("out.txt", "w", stdout);  ///To write  a file.
-//    ios_base::sync_with_stdio(0); cin.tie(0);
-    while(scanf("") ==)
-    {
-
+#ifndef ONLINE_JUDGE
+     freopen("inputf.in", "r", stdin);  ///To read from a file.
+//    freopen("outputf.in", "w", stdout);  ///To write  a file.
+#endif
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int tks=1;
+    //cin>>tks;
+    for(int ks=1;ks<=tks;ks++){
+        //cout<<"Case "<<ks<<": ";
+        solve();
     }
+    //cerr<<"\nTime:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
     return 0;
 }
 ///invisible =  
